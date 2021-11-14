1ed769f64c6098b84714c3b0bf0b4f7636784b46
Alberto <albertomoro002@gmail.com>
2021-11-14 21:28:54 +0100

    all done

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 function compute() {
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    years.type = date.getFullYear();
-    var interest = principal * years + rate / 100;
-    var year = new Date().getFullYear() + parseInt(years);
+    // Get  input values
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
 
-    p = document.getElementById("principal").value;
-    document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + amount + ",\<br\>in the year " + year + "\<br\>"
+    // Validation
+    if (principal <= 0) {
+        alert("Enter a positive number");
+
+        document.getElementById("principal").focus();
+        document.getElementById("principal").click();
+    }
+
+    // Computations
+    interest = principal * years * rate / 100;
+    endYear = new Date().getFullYear() + parseInt(years);
+
+    // Display Result
+    el = document.getElementById("result");
+    el.innerHTML = "If you deposit <span class='highlight'>" + principal + "</span><br/>";
+    el.innerHTML += "at an interest rate of <span class='highlight'>" + rate + "%." + "</span><br/>";
+    el.innerHTML += "You will recieve an amount of <span class='highlight'>" + interest + "</span>,<br/>";
+    el.innerHTML += "in the year <span class='highlight'>" + endYear + "</span>.<br/>";
 }
 
 function updateRate() {
